77932e0655dffe0d8eb0caf1674ce35f5e9a01ee
Raimar <60720672+Raimar123@users.noreply.github.com>
2025-01-21 19:33:16 -0500

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="hidden bg-secondary px-4 py-8 sm:block md:px-8">
       <img className="mx-auto mb-4 aspect-[101/24] max-w-40" src="/img/logo.webp" alt="Logo de AluraFlix" />
-      <p className="text-center text-white/80">Desarrollado con ❤️ por Samuel Gomez - 2025</p>
+      <p className="text-center text-white/80">Desarrollado con ❤️ por Raimar Abarca - 2025</p>
     </footer>
   )
 }
